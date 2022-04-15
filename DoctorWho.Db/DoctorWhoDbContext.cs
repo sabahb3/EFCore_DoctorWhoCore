1ec7d8a4e03c744c54f90bc17851b29c45d88571
sabahb3 <sabahbaara4@gmail.com>
2022-04-15 14:33:59 +0300

    Defining needed DbSets, Renaming models, to begin with, the tbl prefix.

--- DoctorWho.Db/DoctorWhoDbContext.cs
+++ DoctorWho.Db/DoctorWhoDbContext.cs
@@ -4,4 +4,20 @@ namespace DoctorWho.Db;
 
 public class DoctorWhoDbContext : DbContext
 {
+    public DbSet<Episode> tblEpisodes;
+    public DbSet<Author> tblAuthors;
+    public DbSet<Doctor> tblDoctors;
+    public DbSet<Enemy> tblEnemies;
+    public DbSet<Companion> tblCompanions;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>().ToTable("tblAuthor");
+        modelBuilder.Entity<Doctor>().ToTable("tblDoctor");
+        modelBuilder.Entity<Enemy>().ToTable("tblEnemy");
+        modelBuilder.Entity<Companion>().ToTable("tblCompanion");
+        modelBuilder.Entity<Episode>().ToTable("tblEpisode");
+        modelBuilder.Entity<EpisodeCompanion>().ToTable("tblEpisodeCompanion");
+        modelBuilder.Entity<EpisodeEnemy>().ToTable("tblEpisodeEnemy");
+    }
 }
