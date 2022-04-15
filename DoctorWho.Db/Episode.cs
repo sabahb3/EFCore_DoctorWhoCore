b877c11b2633e5c774f70e998a331c428da14806
sabahb3 <sabahbaara4@gmail.com>
2022-04-15 14:50:12 +0300

    Allow null value for note and Episode date

--- DoctorWho.Db/Episode.cs
+++ DoctorWho.Db/Episode.cs
@@ -15,8 +15,8 @@ public class Episode
     public int EpisodeNumber { get; set; }
     public EpisodeType EpisodeType { get; set; }
     public string Title { get; set; }
-    public DateTime EpisodeDate { get; set; }
-    public string Note { get; set; }
+    public DateTime? EpisodeDate { get; set; }
+    public string? Note { get; set; }
     public int AuthorId { get; set; }
     public int DoctorId { get; set; }
 
