9c947dc7835982b62334cdc723d3aa95394daeeb
sabahb3 <sabahbaara4@gmail.com>
2022-04-16 00:06:38 +0300

    Allow null value for Companions and Enemies properties.
    Use string instead of Enum type in EpisodeType property

--- DoctorWho.Db/ViewEpisodes.cs
+++ DoctorWho.Db/ViewEpisodes.cs
@@ -6,12 +6,12 @@ public class ViewEpisodes
     public int EpisodeId { get; set; }
     public int SeriesNumber { get; set; }
     public int EpisodeNumber { get; set; }
-    public EpisodeType EpisodeType { get; set; }
+    public string EpisodeType { get; set; }
     public string Title { get; set; }
     public DateTime? EpisodeDate { get; set; }
     public string? Note { get; set; }
     public string AuthorName { get; set; }
     public string DoctorName { get; set; }
-    public string Companions { get; set; }
-    public string Enemies { get; set; }
+    public string? Companions { get; set; }
+    public string? Enemies { get; set; }
 }
