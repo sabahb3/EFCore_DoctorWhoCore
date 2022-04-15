cde5e6bdc24a01cb84dbd14a63ae903021deab5c
sabahb3 <sabahbaara4@gmail.com>
2022-04-15 18:41:22 +0300

    Adding spaces beween strings

--- DoctorWho.Db/Migrations/20220415142807_AddingView.cs
+++ DoctorWho.Db/Migrations/20220415142807_AddingView.cs
@@ -8,11 +8,11 @@ namespace DoctorWho.Db.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql($"CREATE VIEW viewEpisodes AS"+
-            "SELECT e.EpisodeId,e.SeriesNumber, e.EpisodeNumber,e.EpisodeType,e.Title,e.EpisodeDate,e.Notes,a.AuthorName,"+
-            "d.DoctorName, dbo.fnCompanions(e.EpisodeId) As Companions, dbo.fnEnemies(e.EpisodeId) AS Enemies"+ 
-            "FROM tblEpisode e Left OUTER JOIN tblDoctor d on e.DoctorId=d.DoctorId"+
-            "LEFT OUTER JOIN tblAuthor a on e.AuthorId=a.AuthorId;");
+            migrationBuilder.Sql($"CREATE VIEW viewEpisodes AS "+
+            "SELECT e.EpisodeId,e.SeriesNumber, e.EpisodeNumber,e.EpisodeType,e.Title,e.EpisodeDate,e.Note,a.AuthorName, "+
+            "d.DoctorName, dbo.fnCompanions(e.EpisodeId) As Companions, dbo.fnEnemies(e.EpisodeId) AS Enemies "+ 
+            "FROM tblEpisode e Left OUTER JOIN tblDoctor d on e.DoctorId=d.DoctorId "+
+            "LEFT OUTER JOIN tblAuthor a on e.AuthorId=a.AuthorId; ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
