d6d765d9a87201e2ac886ac89e4f0093721325e0
sabahb3 <sabahbaara4@gmail.com>
2022-04-15 15:05:32 +0300

    Seeding tblEnemy table with 5 records.

--- DoctorWho.Db/DoctorWhoDbContext.cs
+++ DoctorWho.Db/DoctorWhoDbContext.cs
@@ -42,5 +42,44 @@ public class DoctorWhoDbContext : DbContext
         modelBuilder.Entity<EpisodeCompanion>().HasKey(e => new {e.EpisodeId, e.CompanionId});
         modelBuilder.Entity<EpisodeEnemy>().HasKey(e => new {e.EpisodeId, e.EnemyId});
         modelBuilder.Entity<Episode>().Property(e => e.EpisodeType).HasConversion<string>();
+        
+        modelBuilder.Entity<Enemy>().HasData(
+            new Enemy
+            {
+                EnemyId = 1,
+                EnemyName = "Tuberculosis",
+                Description = "Tuberculosis (TB) is caused by bacteria (Mycobacterium tuberculosis) that most often affect" +
+                              " the lungs. Tuberculosis is curable and preventable."
+            },
+            new Enemy
+            {
+                EnemyId = 2,
+                EnemyName = "Plague",
+                Description = "Plague is an infectious disease caused by the bacteria Yersinia pestis, a zoonotic bacteria, usually " +
+                              "found in small mammals and their fleas. It is transmitted between animals through fleas."
+            },
+            new Enemy
+            {
+                EnemyId = 3,
+                EnemyName = "Smallpox",
+                Description = "Smallpox is an ancient disease caused by the variola virus. Early symptoms include high fever and fatigue." +
+                              " The virus then produces a characteristic rash, particularly on the face, arms and legs. The resulting spots " +
+                              "become filled with clear fluid and later, pus, and then form a crust, which eventually dries up and falls off." +
+                              " Smallpox was fatal in up to 30% of cases"
+            },
+            new Enemy
+            {
+                EnemyId = 4,
+                EnemyName = "Poliomyelitis",
+                Description = "A viral infection causing nerve injury which leads to partial or full paralysis. Many of the infected people" +
+                              " do not show any symptoms."
+            },
+            new Enemy
+            {
+                EnemyId = 5,
+                EnemyName = "Cholera",
+                Description = "A bacterial infection which spreads through contaminated food and water."
+            }
+        );
     }
 }
