8636fdd0dda6453202dec1b618dda0c7e1e01ff1
sabahb3 <sabahbaara4@gmail.com>
2022-04-16 00:11:48 +0300

    Writing methods to call Database functions and views

--- DoctorWho/Program.cs
+++ DoctorWho/Program.cs
@@ -1,3 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using DoctorWho.Db;
+using Microsoft.EntityFrameworkCore;
+
+class Program
+{
+    private static DoctorWhoDbContext _context = new DoctorWhoDbContext();
+    public static void Main()
+    {
+        ExecuteView();
+        Console.WriteLine();
+        var companions = GetCompanionsEpisode(1);
+        Console.WriteLine(companions);
+        Console.WriteLine();
+        var enemies = GetEnemiesEpisode(1);
+        Console.WriteLine(enemies);
+    }
+
+    public static void ExecuteView()
+    {
+        var episodesView = _context.ViewEpisodes.ToList();
+    }
+
+    public static string GetCompanionsEpisode(int episodeId)
+    {
+        var result = DoctorWhoDbContext.fnCompanions(episodeId);
+        return result;
+    }
+    
+    public static string GetEnemiesEpisode(int episodeId)
+    {
+        var result = DoctorWhoDbContext.fnEnemies(episodeId);
+        return result;
+    }
+}
