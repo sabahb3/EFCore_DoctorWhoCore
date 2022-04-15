a0ccfaa1133523ddab0c1f642aea1b87b85f69ca
sabahb3 <sabahbaara4@gmail.com>
2022-04-15 16:12:35 +0300

    Fixing error: Dbsets properties not fields

--- DoctorWho.Db/DoctorWhoDbContext.cs
+++ DoctorWho.Db/DoctorWhoDbContext.cs
@@ -5,11 +5,11 @@ namespace DoctorWho.Db;
 
 public class DoctorWhoDbContext : DbContext
 {
-    public DbSet<Episode> tblEpisodes;
-    public DbSet<Author> tblAuthors;
-    public DbSet<Doctor> tblDoctors;
-    public DbSet<Enemy> tblEnemies;
-    public DbSet<Companion> tblCompanions;
+    public DbSet<Episode> tblEpisodes { get; set; }
+    public DbSet<Author> tblAuthors { get; set; }
+    public DbSet<Doctor> tblDoctors { get; set; }
+    public DbSet<Enemy> tblEnemies { get; set; }
+    public DbSet<Companion> tblCompanions { get; set; }
 
     public static readonly ILoggerFactory ConsoleLoggerFactory
         = LoggerFactory.Create(builder =>
@@ -20,15 +20,17 @@ public class DoctorWhoDbContext : DbContext
                     && level == LogLevel.Information)
                 .AddConsole();
         });
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseLoggerFactory(ConsoleLoggerFactory).EnableSensitiveDataLogging()
             .UseSqlServer(
                 "Server=localhost;" +
                 "Database=DoctorWhoCore;" +
-                "Persist Security Info=False;User ID=sa;Password=S.11714778" )
+                "Persist Security Info=False;User ID=sa;Password=S.11714778")
             .UseEnumCheckConstraints();
     }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Author>().ToTable("tblAuthor");
@@ -38,7 +40,7 @@ public class DoctorWhoDbContext : DbContext
         modelBuilder.Entity<Episode>().ToTable("tblEpisode");
         modelBuilder.Entity<EpisodeCompanion>().ToTable("tblEpisodeCompanion");
         modelBuilder.Entity<EpisodeEnemy>().ToTable("tblEpisodeEnemy");
-        
+
         modelBuilder.Entity<EpisodeCompanion>().HasKey(e => new {e.EpisodeId, e.CompanionId});
         modelBuilder.Entity<EpisodeEnemy>().HasKey(e => new {e.EpisodeId, e.EnemyId});
         modelBuilder.Entity<Episode>().Property(e => e.EpisodeType).HasConversion<string>();
