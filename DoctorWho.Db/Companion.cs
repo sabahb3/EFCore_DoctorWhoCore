e94eda235041a203ba5881d3a48052232c1d9e81
sabahb3 <sabahbaara4@gmail.com>
2022-04-15 14:26:01 +0300

    Adding a model called Companion.

---
+++ DoctorWho.Db/Companion.cs
@@ -0,0 +1,8 @@
+namespace DoctorWho.Db;
+
+public class Companion
+{
+    public int CompanionId { get; set; }
+    public string CompanionName { get; set; }
+    public string WhoPlayed { get; set; }
+}
