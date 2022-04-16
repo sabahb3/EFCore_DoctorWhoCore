e4430e90b29c0f530a52b5003eddac388f9a0358
sabahb3 <sabahbaara4@gmail.com>
2022-04-16 15:28:03 +0300

    Writing methods to update existing doctor.

--- DoctorWho/DoctorCUD.cs
+++ DoctorWho/DoctorCUD.cs
@@ -6,8 +6,8 @@ namespace DoctorWho;
 public class DoctorCUD
 {
     private static DoctorWhoDbContext _context = new();
-    
-    public static void CreateDoctor(string doctorNumber, string doctorName,DateTime birthDate)
+
+    public static void CreateDoctor(string doctorNumber, string doctorName, DateTime birthDate)
     {
         var doctor = new Doctor
         {
@@ -18,7 +18,8 @@ public class DoctorCUD
         _context.tblDoctors.Add(doctor);
         _context.SaveChanges();
     }
-    public static void CreateDoctor(string doctorNumber, string doctorName,DateTime birthDate,DateTime firstEpisode)
+
+    public static void CreateDoctor(string doctorNumber, string doctorName, DateTime birthDate, DateTime firstEpisode)
     {
         var doctor = new Doctor
         {
@@ -30,7 +31,9 @@ public class DoctorCUD
         _context.tblDoctors.Add(doctor);
         _context.SaveChanges();
     }
-    public static void CreateDoctor(string doctorNumber, string doctorName,DateTime birthDate,DateTime firstEpisode,DateTime lastEpisode)
+
+    public static void CreateDoctor(string doctorNumber, string doctorName, DateTime birthDate, DateTime firstEpisode,
+        DateTime lastEpisode)
     {
         var doctor = new Doctor
         {
@@ -44,4 +47,123 @@ public class DoctorCUD
         _context.SaveChanges();
     }
 
+    public static void UpdateDoctorNumber(int doctorId, string doctorNumber)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.DoctorNumber = doctorNumber;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctorName(int doctorId, string doctorName)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.DoctorName = doctorName;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctorBirthDate(int doctorId, DateTime birthDate)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.BirthDate = birthDate;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctorFirstEpisode(int doctorId, DateTime firstEpisode)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.FirstEpisodeDate = firstEpisode;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctorLastEpisode(int doctorId, DateTime lastEpisode)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.LastEpisodeDate = lastEpisode;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctor(int doctorId, string doctorNumber, string doctorName)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.DoctorNumber = doctorNumber;
+                doctor.DoctorName = doctorName;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctor(int doctorId, string doctorNumber, string doctorName, DateTime birthDate)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.DoctorNumber = doctorNumber;
+                doctor.DoctorName = doctorName;
+                doctor.BirthDate = birthDate;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctor(int doctorId, string doctorNumber, string doctorName, DateTime birthDate,
+        DateTime firstEpisode)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.DoctorNumber = doctorNumber;
+                doctor.DoctorName = doctorName;
+                doctor.BirthDate = birthDate;
+                doctor.FirstEpisodeDate = firstEpisode;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
+
+    public static void UpdateDoctor(int doctorId, string doctorNumber, string doctorName, DateTime birthDate,
+        DateTime firstEpisode, DateTime lastEpisode)
+    {
+        var doctor = _context.tblDoctors.Find(doctorId);
+        if (doctor != null)
+            using (var updateContext = new DoctorWhoDbContext())
+            {
+                doctor.DoctorNumber = doctorNumber;
+                doctor.DoctorName = doctorName;
+                doctor.BirthDate = birthDate;
+                doctor.FirstEpisodeDate = firstEpisode;
+                doctor.LastEpisodeDate = lastEpisode;
+                updateContext.Entry(doctor).State = EntityState.Modified;
+                updateContext.SaveChanges();
+            }
+    }
 }
