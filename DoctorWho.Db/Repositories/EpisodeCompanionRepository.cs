4ef5865c1c27595e7a211ca08ea1f196fe6881b9
sabahb3 <sabahbaara4@gmail.com>
2022-04-17 12:39:25 +0300

    Adding a repository for the EpisodeCompanion' methods

---
+++ DoctorWho.Db/Repositories/EpisodeCompanionRepository.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db.Repositories;
+
+public class EpisodeCompanionRepository
+{
+    private DoctorWhoDbContext _context;
+
+    public EpisodeCompanionRepository(DoctorWhoDbContext context)
+    {
+        _context = context;
+    }
+    public void AddCompanionToEpisode(int episodeId, string companionName, string whoPlayed)
+    {
+        var episode = _context.tblEpisodes.Find(episodeId);
+        if (episode != null)
+        {
+            var companion = new Companion
+            {
+                CompanionName = companionName,
+                WhoPlayed = whoPlayed
+            };
+            using (var addCompanion = new DoctorWhoDbContext(_context.DoctorWhoOptions.Options))
+            {
+                addCompanion.Attach(episode);
+                episode.EpisodesCompanions.Add
+                (
+                    new EpisodeCompanion
+                    {
+                        Companion = companion
+                    }
+                );
+                addCompanion.SaveChanges();
+            }
+        }
+    }
+
+}
--- DoctorWho/Program.cs
+++ DoctorWho/Program.cs
@@ -62,32 +62,6 @@ internal class Program
         return result;
     }
 
-
-    public static void AddCompanionToEpisode(int episodeId, string companionName, string whoPlayed)
-    {
-        var episode = _context.tblEpisodes.Find(episodeId);
-        if (episode != null)
-        {
-            var companion = new Companion
-            {
-                CompanionName = companionName,
-                WhoPlayed = whoPlayed
-            };
-            using (var addCompanion = new DoctorWhoDbContext())
-            {
-                addCompanion.Attach(episode);
-                episode.EpisodesCompanions.Add
-                (
-                    new EpisodeCompanion
-                    {
-                        Companion = companion
-                    }
-                );
-                addCompanion.SaveChanges();
-            }
-        }
-    }
-
     public static void GetAllDoctor()
     {
         var doctors = _context.tblDoctors.ToList();
